acca9f394430f74d2851a648c61a0ce7b0befc9f
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-25 19:07:51 +0100

    Completato il progetto di base

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.- PROGRAMMA PRINCIPALE -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+using Geometria;
+// ISTANZIO 1 rettangolo
+
+Rettangolo rettangolo1 = new Rettangolo();
+
+string myString = "";
+int intValue;
+bool controllo;
+
+Console.Write("Inserisci il valore per la base del rettangolo: ");
+do
+{
+    myString = Console.ReadLine();
+    if (int.TryParse(myString, out rettangolo1.baseRettangolo))
+    {
+        controllo = true;
+    }
+    else
+    {
+        Console.WriteLine("\nHai inserito un valore errato!!!");
+        controllo = false;
+        Console.Write("\nInserisci UN NUMERO per define la base del rettangolo: ");
+    }
+} while (!controllo);
+
+Console.WriteLine("[ Base rettangolo - " + rettangolo1.baseRettangolo + " cm - ]\n");
+
+
+Console.Write("Inserisci il valore per la altezza del rettangolo: ");
+do
+{
+    myString = Console.ReadLine();
+    if (int.TryParse(myString, out rettangolo1.altezzaRettangolo))
+    {
+        controllo = true;
+    }
+    else
+    {
+        Console.WriteLine("\nHai inserito un valore errato!!!");
+        controllo = false;
+        Console.Write("\nInserisci UN NUMERO per define la altezza del rettangolo: ");
+    }
+} while (!controllo);
+
+Console.WriteLine("[ Altezza rettangolo - " + rettangolo1.altezzaRettangolo + " cm - ]\n");
+
+
+rettangolo1.calcolaPerimetro();
+
+rettangolo1.calcolaArea();
+
+
+
--- Geometria/Rettangolo.cs
+++ Geometria/Rettangolo.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Geometria
 {
-    internal class Rettangolo
+    public class Rettangolo
     {
+        // CARATTERISTICHE
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+        // STATO
+        public int areaRettangolo;
+        public int perimetroRettangolo;
+
+        // METODI
+        public void calcolaArea()
+        {
+            areaRettangolo = baseRettangolo * altezzaRettangolo;
+            Console.WriteLine("L'area del rettangolo è: " + areaRettangolo + " cm2");
+        }
+        public void calcolaPerimetro()
+        {
+            perimetroRettangolo = (baseRettangolo + altezzaRettangolo) * 2;
+            Console.WriteLine("\nIl perimetro del rettangolo è: " + perimetroRettangolo + " cm");
+        }
+
+
+        public void StampaAuto()
+        {
+            Console.WriteLine("Base: \t" + baseRettangolo + " cm");
+            Console.WriteLine("Altezza: \t" + altezzaRettangolo + " cm");
+            Console.WriteLine("Perimetro: \t" + perimetroRettangolo + " cm");
+            Console.WriteLine("Area: \t" + areaRettangolo + " cm2");
+        }
+
     }
 }
